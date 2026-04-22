70d55a43e6a5974b43768beebfd7266be393a11c
Agustin Rodriguez <agustinlihuel@gmail.com>
2026-04-21 23:17:06 -0300

    express

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 import Usuario from "./Usuario.js"
+import express from "express"
 
 
-const usuario_uno = new Usuario("Carlos", "carlitos@gmail", "777888")
+const mi_servidor = express()
 
 
-usuario_uno.saludar()
-usuario_uno.getPassword()
+//end points get/post etc
+mi_servidor.get("/", (req, res) => {
+    res.send("Data")
+ })
+
+mi_servidor.get("/usuarios", (req, res) => {
+    res.send([{id:1, nombre:"luis",}, {id:2, nombre:"carlos"}])
+ })
+
+
+// servidor (puerto, callback)
+mi_servidor.listen(8080, () => {
+    console.log("server up and running !")
+})
+
+// const usuario_uno = new Usuario("Carlos", "carlitos@gmail", "777888")
+
+
+// usuario_uno.saludar()
+// usuario_uno.getPassword()
